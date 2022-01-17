88eff1f10b8e1e8b4ae9ddab1c83b701b22f4b5f
João Paulo Sousa <jpaulo.vmsousa@yahoo.com.br>
2022-01-17 01:30:09 -0300

    Adicionar operações de Sacar, Depositar e Transferir

--- src/main/java/Conta.java
+++ src/main/java/Conta.java
@@ -34,6 +34,38 @@ public abstract class Conta {
         this.saldo = saldo;
     }
 
+    public void depositar(Double valor) throws Exception {
+        if (valor <= 0)
+            throw new Exception("Operação Cancelada! Inclua um valor maior do que zero (0)");
+        this.saldo += valor;
+    }
+
+    public void sacar(Double valor) throws Exception {
+        if (valor <= 0)
+            throw new Exception("Operação Cancelada! Inclua um valor maior do que zero (0)");
+        if (Double.compare(valor, this.saldo) == 1)
+            throw new Exception("Operação Cancelada! Você não possui saldo suficiente.");
+        this.saldo -= valor;
+    }
+
+    public void transferir(Conta cDestino, Double valor) {
+        try {
+            this.sacar(valor);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            cDestino.depositar(valor);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            try {
+                this.depositar(valor);
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
     @Override
     public String toString() {
         return "{" +
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         Conta c1 = new ContaCorrente("1234-5", "12345-6");
+        Conta c2 = new ContaPoupanca("5432-1", "65432-1");
         System.out.println(c1);
+        System.out.println(c2);
+        c1.depositar(100d);
+        System.out.println(c1);
+        c1.transferir(c2, 50d);
+        System.out.println("Conta 1: " + c1);
+        System.out.println("Conta 2: " + c2);
     }
 }
